6dd1fa8ea0a5e88abce0bb0c169a3b81518ded14
Roy <roy200230034004@gmail.com>
2019-05-02 01:46:26 +0800

    add vue

--- Scripts/test1.js
+++ Scripts/test1.js
@@ -1,3 +1,21 @@
-var a = 123;
 
-	console.log(a);
+ var app = new Vue({
+ 	el:'#app',
+ 	data: {
+ 		topic:"HELLO",
+ 		subTopic: "VUE"
+ 	},
+ 	computed:{
+ 		title:{
+ 			get: function(){
+ 				return `${this.topic}    ${this.subTopic}`  ;
+ 			}
+ 		}
+ 	},
+ 	methods:{
+
+
+ 	},
+
+
+ })
